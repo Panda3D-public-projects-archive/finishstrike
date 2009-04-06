8a9db1011dc8233f38586be690f18c0152541aa6
lopes.maycon <lopes.maycon@e3265f6f-5d53-0410-85ac-b1078a54aeeb>
2009-04-06 22:45:54 +0000

    replaced TAB identation by four spaces

--- aplicacao/framework/GraphFacade.py
+++ aplicacao/framework/GraphFacade.py
@@ -1,7 +1,7 @@
 from Graph import *
 
 class GraphFacade():
-	""" makes easy to use the Graph class """
-	def getGraph(self, vertex_path, edge_path):
-		""" returns a new graph """
-		return Graph(vertex_path, edge_path)
+    """ makes easy to use the Graph class """
+    def getGraph(self, vertex_path, edge_path):
+        """ returns a new graph """
+        return Graph(vertex_path, edge_path)
