2aacef269628fca2ed8945170d3189cd3d8ff5b4
lopes.maycon <lopes.maycon@e3265f6f-5d53-0410-85ac-b1078a54aeeb>
2009-05-01 00:55:47 +0000

    robot atualization

--- aplicacao/robot.py
+++ aplicacao/robot.py
@@ -1,18 +1,19 @@
 from direct.actor.Actor import Actor
 
 class Robot():
-	""" this class leads with the actor, the robot """
+    """ this class leads with the actor, the robot """
 
-	def __init__(self):
+    def __init__(self):
 
-		self.odometer = 0
-		# load the actor
-		self.robot = Actor("./modelos/r2d2.egg")
-		#self.robot = Actor("./modelos/ralph.egg.pz",
-		#										{"run":"./modelos/ralph-run.egg.pz",
-		#											"walk":"./modelos/ralph-walk.egg.pz"})
-		# load the texture
-		self.tex = loader.loadTexture("modelos/r2.png")
-		#self.tex = loader.loadTexture("modelos/ralph.jpg")
-		# set the texture loaded
-		self.robot.setTexture(self.tex, 1)
+        self.odometer = 0
+        # load the actor
+        self.robot = Actor("./modelos/r2d2.egg")
+        #self.robot = Actor("./modelos/ralph.egg.pz")
+        # self.robot = Actor("./modelos/ralph.egg.pz",
+                            # {"run":"./modelos/ralph-run.egg.pz",
+                            # "walk":"./modelos/ralph-walk.egg.pz"})
+        # load the texture
+        #self.tex = loader.loadTexture("modelos/ralph.jpg")
+        self.tex = loader.loadTexture("modelos/r2.png")
+        # set the texture loaded
+        self.robot.setTexture(self.tex, 1)
