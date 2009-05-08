7a26045fce552e934bfb3c4b5d05b7929c6ea73a
lopes.maycon <lopes.maycon@e3265f6f-5d53-0410-85ac-b1078a54aeeb>
2009-05-08 01:03:26 +0000

    new method: createLandmarkGraph

--- aplicacao/framework/SLAM.py
+++ aplicacao/framework/SLAM.py
@@ -1,9 +1,15 @@
+from math_module import *
+
 class Slam():
 
     def __init__(self):
+        self.infinity_point = 9999
         self.bigger_key = None
         self.bigger_value = None
         self.landmark_dic = {}
+        # this is a list of tuples like [(x1,y1),...,(xn,yn)]
+        self.landmark_graph = []
+        self.trigonometry_obj = Trigonometry()
     def biggerDictionaryKey(self, dictionary):
         """ gets bigger key in dictionary """
         #bigger value calculation
@@ -25,4 +31,16 @@ class Slam():
         
     def landmarkUpdate(self, key, value):
         self.landmark_dic[key] = value
+     
+    def createLandmarkGraph(self, angle):
+        """ creates a landmark Graph using mathematical 
+            calculations of rectangle triangle
+        """
+        hypotenuse = self.landmark_dic[angle]
+        angle = float(angle)
+        x = self.trigonometry_obj.getXCateto(hypotenuse, angle)
+        y = self.trigonometry_obj.getYCateto(hypotenuse, angle)
+        tuple = (x, y)
+        if tuple not in self.landmark_graph and hypotenuse != self.infinity_point:
+            self.landmark_graph.append(tuple)
 
