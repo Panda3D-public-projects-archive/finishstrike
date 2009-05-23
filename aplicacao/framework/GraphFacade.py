94c1d9403fd188ec1ca6147222c44a06d0c317db
lucascarvalho <lucascarvalho@e3265f6f-5d53-0410-85ac-b1078a54aeeb>
2009-05-23 19:41:14 +0000

    Removed old version

--- aplicacao/framework/GraphFacade.py
+++
@@ -1,7 +0,0 @@
-from Graph import *
-
-class GraphFacade():
-    """ makes easy to use the Graph class """
-    def getGraph(self, vertex_path, edge_path):
-        """ returns a new graph """
-        return Graph(vertex_path, edge_path)
