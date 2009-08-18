bda06a6d41ffeacbfd0a8639d6c4d304a7f887b2
lucascarvalho <lucascarvalho@e3265f6f-5d53-0410-85ac-b1078a54aeeb>
2009-08-18 04:45:11 +0000

    added new urls, added new cars objects and added new templates for car model.

--- anpr_django/urls.py
+++ anpr_django/urls.py
@@ -4,14 +4,11 @@ from django.conf import settings
 from anpr.models import Person, Car
 
 # Uncomment the next two lines to enable the admin:
-#from django.contrib import admin
-#admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 extra_context = {
- 'people_length': len(Person.objects.all())
-}
-
-car_extra_context = {
+ 'people_length': len(Person.objects.all()),
  'car_length': len(Car.objects.all())
 }
 
@@ -21,80 +18,88 @@ urlpatterns = patterns('',
                      {'document_root': settings.MEDIA_ROOT}),
 
     # Example:list_detail.object_list
-    # (r'^anpr/', include('anpr.foo.urls')),
+#     (r'^anpr/', include('anpr.foo.urls')),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/(.*)', admin.site.root),
+    (r'^admin/(.*)', admin.site.root),
 
     #Person url's
     (r'^person/list/?$', 'django.views.generic.list_detail.object_list',
-            dict({'queryset': Person.objects.all(),
-                  'template_object_name': 'person',
-                  'paginate_by': 10,
-                  'template_name': 'person/person_list.html'},
-                 extra_context=extra_context)),
+      dict({'queryset': Person.objects.all(),
+            'template_object_name': 'person',
+            'paginate_by': 10,
+            'template_name': 'person/person_list.html'},
+            extra_context=extra_context)),
 
-    (r'^person/view/(?P<object_id>\d+)/?$', 'django.views.generic.list_detail.object_detail',
-            dict({'queryset': Person.objects.all(),
-                 'template_object_name': 'person',
-                 'template_name': 'person/person_view.html'},
-                 extra_context=extra_context)),
+    (r'^person/view/(?P<object_id>\d+)/?$', 
+                           'django.views.generic.list_detail.object_detail',
+      dict({'queryset': Person.objects.all(),
+            'template_object_name': 'person',
+            'template_name': 'person/person_view.html'},
+            extra_context=extra_context)),
 
     (r'^person/create/?$', 'django.views.generic.create_update.create_object',
-                    dict({'model': Person, 
-                          'template_name': 'person/person_form.html'}, 
-                          extra_context=extra_context,
-                          post_save_redirect="/person/list/") ),
+      dict({'model': Person, 
+            'template_name': 'person/person_form.html'}, 
+            extra_context=extra_context,
+            post_save_redirect="/person/list/") ),
 
     (r'^person/update/(?P<object_id>\d+)/?$', 
       'django.views.generic.create_update.update_object',
       dict({'model': Person,
             'template_name': 'person/person_form.html'},
             extra_context=extra_context,
-            post_save_redirect="/person/view/")),
+            post_save_redirect="/person/view/%(id)s")),
 
     (r'^person/delete/(?P<object_id>\d+)/?$',
-      'django.views.generic.create_update.delete_object',
+                        'django.views.generic.create_update.delete_object',
       dict({'model': Person, 
             'template_object_name': 'person',
             'template_name': 'person/person_confirm_delete.html'}, 
             extra_context=extra_context,
             post_delete_redirect="/person/list/")),
 
-   # XXX - it is not required for now.
-   # (r'^person/search/$','anpr_django.anpr.views.search'),
+    # XXX - it is not required for now.
+    # (r'^person/search/$','anpr_django.anpr.views.search'),
 
-   #Car url's
+    # Car url's
     (r'^car/create/?$','django.views.generic.create_update.create_object',
-                    dict({'model': Car,
-                          'template_name': 'car/car_form.html'},
-                          extra_context=car_extra_context,
-                          post_save_redirect='/car/list/')),
+      dict({'model': Car,
+            'template_name': 'car/car_form.html'},
+            extra_context=extra_context,
+            post_save_redirect='/car/list/')),
     
-    (r'car/update/(?P<object_id>\d+)/?$', 
-        'django.views.generic.create_update.update_object',
-        dict({'model': Car,
-              'template_name': 'person/car_form.html'},
-              extra_context=car_extra_context)),
+    (r'^car/update/(?P<object_id>\d+)/?$', 
+                        'django.views.generic.create_update.update_object',
+      dict({'model': Car,
+            'template_name': 'car/car_form.html'},
+            extra_context=extra_context,
+            post_save_redirect="/car/view/%(id)s")),
 
     (r'^car/delete/(?P<object_id>\d+)/?$',
-      'django.views.generic.create_update.delete_object',
+                       'django.views.generic.create_update.delete_object',
       dict({'model':Car,
             'template_name': 'car/car_confirm_delete.html'},
-            extra_context=car_extra_context,
+            extra_context=extra_context,
             post_delete_redirect='car/list/')),
 
+    (r'^car/list/?$', 'django.views.generic.list_detail.object_list',
+      dict({'queryset': Car.objects.all(),
+           'template_object_name': 'car',
+           'paginate_by': 10,
+    	     'template_name': 'car/car_list.html',},
+    	    extra_context=extra_context)),
 
-    #(r'^car/list/?$', 'django.views.generic.list_detail.object_list',
-    #  dict({'model': Car,
-    #	     'template_name': 'person/car_list.html',
-    #	     'paginate_by': 10,	
-    #	},
-    #	    extra_context=car_extra_context)),
-    (r'^car/list/?$', 'anpr_django.anpr.views.car_list'),
+    (r'^car/view/(?P<object_id>\d+)/?$', 
+                        'django.views.generic.list_detail.object_detail',
+      dict({'queryset': Car.objects.all(),
+            'template_object_name': 'car',
+            'template_name': 'car/car_view.html'},
+            extra_context=extra_context)),
+
     
 )
